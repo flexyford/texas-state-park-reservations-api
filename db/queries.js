1d77c8f68fbf957ea55a970fb7afab1635a41623
Alex Ford <alexford@frontside.io>
2017-02-13 19:04:02 -0600

    Scrape 6 Months worth of park info

--- db/queries.js
+++ db/queries.js
@@ -24,6 +24,12 @@ function getSinglePark(id) {
     .first();
 }
 
+function getSingleParkBy(attributes = {}) {
+  return Parks()
+    .where(attributes)
+    .first();
+}
+
 function _getSitesForParks(parkIds) {
   return Sites()
     .whereIn('park_id', parkIds)
@@ -35,9 +41,21 @@ function getAllSites() {
   return Sites().select();
 }
 
+function getSitesBy(attributes = {}) {
+  return Sites()
+    .where(attributes)
+    .select();
+}
+
 function getSingleSite(id) {
   return Sites()
     .where('id', parseInt(id))
+    .first();
+}
+
+function getSingleSiteBy(attributes = {}) {
+  return Sites()
+    .where(attributes)
     .first();
 }
 
@@ -48,9 +66,25 @@ function _getCampDatesForSites(siteIds) {
     .map(({ id, site_id }) => { return { id, site_id }; });
 }
 
+function getAllCampDates() {
+  return CampDates().select();
+}
+
+function getCampDatesBy(attributes = {}) {
+  return CampDates()
+    .where(attributes)
+    .select();
+}
+
 function getSingleCampDate(id) {
   return CampDates()
     .where('id', parseInt(id))
+    .first();
+}
+
+function getSingleCampDateBy(attributes = {}) {
+  return CampDates()
+    .where(attributes)
     .first();
 }
 
@@ -58,9 +92,15 @@ function getSingleCampDate(id) {
 module.exports = {
   getAllParks,
   getSinglePark,
+  getSingleParkBy,
   _getSitesForParks,
   getAllSites,
+  getSitesBy,
   getSingleSite,
+  getSingleSiteBy,
   _getCampDatesForSites,
-  getSingleCampDate
+  getAllCampDates,
+  getCampDatesBy,
+  getSingleCampDate,
+  getSingleCampDateBy
 };
---
+++ scraper/dates.js
@@ -0,0 +1,201 @@
+var moment = require('moment');
+var request = require('request');
+var cheerio = require('cheerio');
+
+var knex = require('../db/knex.js');
+let queries = require('../db/queries');
+
+const ROW = {
+  TYPE: 0,
+  DATES_START: 5,
+  DATES_END: 19
+};
+
+const DAYS_PER_CALENDAR_VIEW = 14;
+
+function findSiteBy(attributes = {}) {
+  return queries.getSingleSiteBy(attributes);
+}
+
+function getRowData($row, start, end) {
+  if (!end) {
+    return $row.find('td').eq(start).text();
+  } else {
+    let $ = this;
+
+    return $row.find('td').slice(start, end).map(function(i, e) {
+      return $(this).text();
+    }).toArray();
+  }
+}
+
+function createOrUpdateCampDate(attributes) {
+  let { date, available, site_id } = attributes;
+  let campDateQuery = queries.getSingleCampDateBy({
+    date, site_id
+  });
+
+  return campDateQuery.then((campDate) => {
+    if (!campDate) {
+      return knex('camp_dates').insert({
+        date: moment(date).format(),
+        available,
+        site_id
+      });
+    } else if (parseInt(campDate.available) !== parseInt(available)) {
+      return campDateQuery.update({
+        available,
+        updated_at: knex.fn.now()
+      });
+    } else {
+      return campDate;
+    }
+  });
+}
+
+function parseRow($row, $header, reserveWorldCampId) {
+  let $ = this;
+  let type = getRowData.call($, $row, ROW.TYPE);
+  let siteAttrs = { type,  reserveworld_camp_id: reserveWorldCampId };
+
+  return findSiteBy(siteAttrs).then((site) => {
+
+    let campDates = getRowData.call($, $header, ROW.DATES_START, ROW.DATES_END).map((date, i) => {
+      let available = getRowData.call($, $row, ROW.DATES_START + i);
+
+      return createOrUpdateCampDate({
+        date: moment(date, 'MM/DD').format('YYYY-MM-DD'),
+        available: available,
+        site_id: site.id
+      });
+    });
+
+    return Promise.all(campDates);
+  });
+}
+
+function parseCalendar(url, html) {
+  let reserveWorldId = url.match(/campId=(\d+)/).pop();
+
+  let $ = cheerio.load(html);
+
+  let $table = $('table[summary]');
+  let $rows = $table.find('tr');
+
+  let $header = $rows.eq(1);
+
+  let promiseDates = $rows.slice(2).map(function() {
+    return parseRow.call($, $(this), $header, reserveWorldId);
+  }).toArray();
+
+  return Promise.all(promiseDates);
+}
+
+function requestCalendar(url) {
+  return new Promise(function(resolve, reject) {
+    request(url, function(error, response, html) {
+      if(!error) {
+        resolve(html);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+function requestCalendars(urls) {
+  let requests = urls.map((url) => {
+    return requestCalendar(url)
+      .then((html) => {
+        // console.log('parsing: ', url);
+        return parseCalendar(url, html);
+      })
+      .catch((error) => {
+        console.log("Error: ", error, ' ', url);
+      });
+  });
+
+  return Promise.all(requests)
+    .catch((error) => {
+      console.log("Error: ", error);
+    });
+}
+
+function buildReservationsForCamp(campId, arrivalDate, departureDate) {
+  const calendarUrl = `http://texas.reserveworld.com/GeneralAvailabilityCalendar.aspx`;
+
+  let from = moment(arrivalDate);
+  let to = moment(departureDate);
+  let duration = to.diff(from, 'days');
+
+  let numViews = Math.ceil(duration / DAYS_PER_CALENDAR_VIEW);
+
+  let arrivalDates = new Array(numViews).fill( null ).map((_, index) => {
+    let from = moment(arrivalDate);
+    let days = DAYS_PER_CALENDAR_VIEW * index;
+    return from.add(days, 'days');
+  });
+
+  let urls = arrivalDates.map((date) => {
+    let arrivalDate = date.format('MM/DD/YYYY');
+    return calendarUrl + `?campId=${campId}&arrivalDate=${arrivalDate}`;
+  });
+
+  return requestCalendars(urls);
+}
+
+function buildReservationsForPark(park, arrivalDate, departureDate) {
+  let reserveworldCampIds = park.reserveworld_camp_ids ?
+    park.reserveworld_camp_ids.split(',') : [];
+
+  return Promise.all(
+    reserveworldCampIds.map((reserveworldCampId) => {
+      return buildReservationsForCamp(
+        reserveworldCampId, arrivalDate, departureDate
+      );
+    })
+  );
+}
+
+function buildReservationsForParks(parks, arrivalDate, departureDate) {
+  return Promise.all(
+    parks.map((park) => {
+      return buildReservationsForPark(park, arrivalDate, departureDate);
+    })
+  );
+}
+
+function requestParks(parks, size) {
+  let batch = parks.splice(0, size || parks.length);
+  let batchRequests = batch.map((park) => {
+    return buildReservationsForPark(park, '2017-02-13', '2017-08-13');
+  });
+  return Promise.all(batchRequests).then((results) => {
+    // console.log('Success: ', batch);
+    if (parks.length > 0) {
+      return requestParks(parks, size);
+    }
+    return results;
+  });
+}
+
+function scrapePark() {
+  return queries.getSingleParkBy({name: 'BRAZOS BEND SP'}).then((park) => {
+    return requestParks([ park ]);
+  });
+}
+
+function scrapeParks() {
+  return queries.getAllParks().then((parks) => {
+    return requestParks(parks, 10);
+  });
+}
+
+scrapeParks().then((data) => {
+  process.exit();
+});
+
+// queries.getCampDatesBy({ date: '2017-02-14' }).limit(10).then((dates) => {
+//   console.log('dates = ', dates);
+//   process.exit();
+// });
